83570e39329dd824e26c87d6e0f8e962c9a136cf
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-04-11 09:03:05 +0100

    tweak myocamlbuild

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,7 @@
 open Ocamlbuild_plugin
 
-let pkg_ctypes = Findlib.query "ctypes"
+let ctypes_lib = let ctypes = Findlib.query "ctypes" in ctypes.Findlib.location
+let ocaml_stdlib = run_and_read "ocamlfind printconf stdlib"
 let cc = "cc" (* C compiler *)
 
 let () =
@@ -29,8 +30,8 @@ let () =
           ~prods:["lib/gccjit_stubs_generated.ml"; "lib/gccjit_stubs.c"]
           (fun _ _ -> Cmd (S [P "lib_gen/gen_stubs.byte"]));
 
-        flag [ "compile"; "c"; "use_ctypes_c_headers" ] (S [A "-I"; P pkg_ctypes.Findlib.location]);
-        flag [ "compile"; "c"; "use_ocaml_c_headers" ] (S [A "-I"; P (pkg_ctypes.Findlib.location ^ "/../ocaml")]);
+        flag [ "compile"; "c"; "use_ctypes_c_headers" ] (S [A "-I"; P ctypes_lib]);
+        flag [ "compile"; "c"; "use_ocaml_c_headers" ] (S [A "-I"; P ocaml_stdlib]);
 
         dep [ "ocaml"; "link"; "use_gccjit" ] [ "lib/gccjit_stubs.o" ];
         flag [ "ocaml"; "link"; "use_gccjit" ] (S [A"-cclib"; P"-lgccjit"]);
