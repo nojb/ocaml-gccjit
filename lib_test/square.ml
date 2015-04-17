022df006fde760742026d16550e9f69661169ff8
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-04-17 22:43:33 +0100

    Update lib_test/square.ml

--- lib_test/square.ml
+++ lib_test/square.ml
@@ -1,38 +1,36 @@
-open Gccjit
+module G = Gccjit.Make ()
+
+open G
 
 let main () =
-  (* Create a compilation context *)
-  let ctx = acquire () in
-
   (* Turn these on to get various kinds of debugging *)
-  (* set_option ctx Dump_initial_tree true; *)
-  (* set_option ctx Dump_initial_gimple true; *)
-  (* set_option ctx Dump_generated_code true; *)
+  Context.set_option Context.Dump_initial_tree true;
+  Context.set_option Context.Dump_initial_gimple true;
+  Context.set_option Context.Dump_generated_code true;
 
   (* Adjust this to control optimization level of the generated code *)
-  (* set_option ctx Optimization_level 3; *)
-
-  let int_type = get_standard_type ctx Int in
+  Context.set_option Context.Optimization_level 3;
 
   (* Create parameter "i" *)
-  let param_i = new_param ctx int_type "i" in
+  let param_i = Param.create Type.int "i" in
 
   (* Create the function *)
-  let fn = new_function ctx Exported int_type "square" [ param_i ] in
+  let fn = Function.create Function.Exported Type.int "square" [ param_i ] in
 
   (* Create a basic block within the function *)
-  let block = new_block fn ~name:"entry" () in
+  let block = Block.create fn in
 
   (* This basic block is relatively simple *)
-  let expr = new_binary_op ctx Mult int_type param_i param_i in
-  end_with_return block expr;
+  let expr = RValue.binary_op Mult Type.int (RValue.param param_i) (RValue.param param_i) in
+
+  Block.return block expr;
 
   (* Having populated the context, compile it *)
-  let jit_result = compile ctx in
+  let res = Context.compile () in
 
   (* Look up a specific machine code routine within the gccjit.Result, in this
      case, the function we created above: *)
-  let callable = get_code jit_result "square" Ctypes.(int @-> returning int) in
+  let callable = Result.code res "square" Ctypes.(int @-> returning int) in
 
   (* Now try running the code *)
   assert (25 = callable 5)
